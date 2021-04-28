8b0d956a6ff1165051f689f1fec030cbaa494e5d
jithish <63664628+jithish-sekar@users.noreply.github.com>
2021-04-28 18:17:51 +0530

    Update Project_component.js

--- src/components/project/Project_component.js
+++ src/components/project/Project_component.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import BaseComponent from "../base/Base_component";
 import "./Project_component.scss";
-import {Link } from 'react-router-dom';
 function ProjectComponent() {
   useEffect(() => {
     window.scrollTo(0, 0);
